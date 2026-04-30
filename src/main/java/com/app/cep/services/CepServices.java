2e999c3b8c02dcad2f82ebb4b97a74920db70ddc
maikon.muniz <maikon.muniz@yahoo.com.br>
2026-04-30 20:07:20 -0300

    feat: adicionei repository

--- src/main/java/com/app/cep/services/CepServices.java
+++ src/main/java/com/app/cep/services/CepServices.java
@@ -1,25 +1,30 @@
 package com.app.cep.services;
 
+import com.app.cep.dto.EnderecoDTO;
 import com.app.cep.infra.exception.ExceptionAdapter;
 import com.app.cep.infra.http.Api;
+import com.app.cep.infra.repository.RepositoryAnddress;
 
 public class CepServices {
 
     private final Api api;
     private final ExceptionAdapter exceptionAdapter;
+    private final RepositoryAnddress repoAnddress;
 
-    public CepServices (Api api, ExceptionAdapter exceptionAdapter) {
+    public CepServices (Api api, ExceptionAdapter exceptionAdapter, RepositoryAnddress repositoryAnddress) {
         this.api = api;
         this.exceptionAdapter = exceptionAdapter;
+        this.repoAnddress = repositoryAnddress;
     }
 
-    public String execute (String cep) {
+    public EnderecoDTO execute (String cep) {
         if (cep == null || cep.isEmpty()) throw this.exceptionAdapter.badRequest("Campo cep está em branco!");
         if (!cep.matches("\\d+")) throw this.exceptionAdapter.badRequest("Campo cep deve conter apenas números!");
         if (cep.length() != 8) throw this.exceptionAdapter.badRequest("Campo cep deve ter 8 dígitos!");
 
         try {
-            return this.api.get(cep);
+            EnderecoDTO enderecoDTO = this.api.get(cep);
+            return repoAnddress.save(enderecoDTO);
         } catch (Exception exception) {
             throw this.exceptionAdapter.internalServe("Erro ao processar o CEP: " + exception.getMessage());
         }
