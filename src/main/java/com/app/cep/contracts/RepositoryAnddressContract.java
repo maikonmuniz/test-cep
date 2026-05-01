5d4d4314020c8c6a97ef5023afe2863c3129d8d5
maikon.muniz <maikon.muniz@yahoo.com.br>
2026-05-01 00:02:14 -0300

    feat: atualização o nome do retorno

--- src/main/java/com/app/cep/contracts/RepositoryAnddressContract.java
+++ src/main/java/com/app/cep/contracts/RepositoryAnddressContract.java
@@ -1,7 +1,7 @@
 package com.app.cep.contracts;
 
-import com.app.cep.dto.EnderecoDTO;
+import com.app.cep.domain.Endereco;
 
 public interface RepositoryAnddressContract {
-    EnderecoDTO save (EnderecoDTO enderecoDTO);
+    Endereco save (Endereco endereco);
 }
